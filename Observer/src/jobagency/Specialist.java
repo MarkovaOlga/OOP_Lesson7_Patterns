5026087b339bfd8d0b19da79617f59cce1d6bc8d
MarkovaOlga <olgamarkova85@ya.ru>
2023-06-28 15:22:31 +0300

    add specialist

---
+++ Observer/src/jobagency/Specialist.java
@@ -0,0 +1,17 @@
+package jobagency;
+
+public class Specialist implements Observer{
+    String name;
+    public Specialist(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void receiveOffer(String nameCompany, int salary) {
+        if (nameCompany.equalsIgnoreCase("Google")) {
+            System.out.println(String.format("Specialist %s: Ok. I am ready for this job! (company, salary) = %s, %d", name, nameCompany, salary));
+        } else {
+            System.out.println(String.format("Specialist %s: I am waiting for the better job! (company, salary) = %s, %d", name, nameCompany, salary));
+        }
+    }
+}
